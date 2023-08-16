3cd315aab8529f278e0d867a073475c21af58371
Gustave <apps@gusp.io>
2023-08-16 11:18:30 +0200

    Initial commit

---
+++ test/aws-cdk-boiterplate.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkBoiterplate from '../lib/aws-cdk-boiterplate-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-boiterplate-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkBoiterplate.AwsCdkBoiterplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
